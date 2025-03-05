8fdd1803529f0684a37e3b031da418eac6f987ca
Phuwizra <65200275@kmitl.ac.th>
2025-03-05 22:00:38 +0700

    Add Database Map new volume make some change in api

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express  = require('express')
+const bodyparser  = require('body-parser')
+const mysql = require('mysql2/promise')
 const app = express()
 
 const port = 8000
+
 
 app.get('/hello_world',(req,res) => {
     res.send('hello world')
 })
 
 app.get('/showscore',(req,res) => {
-    res.send()
+    mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'Karma'
+    }).then((data) => {
+        data
+        .query('SELECT * FROM')
+        .then((results) => {
+            res.json(results[0])
+    })
+    .catch((error) => {
+        console.error('Error fetching users:', error.message)
+        res.status(500).json({ error: 'Error fetching users' })
+        })
+    })
 })
 
 app.get('/showscorefull',(req,res) => {
